40ac312d822105dc3d8688af6b8c957f1145355b
hmouda12 <92645409+hmouda12@users.noreply.github.com>
2021-10-19 22:05:18 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+function genimg(){
+    var img = document.createElement('img');
+    var div = document.getElementById('cont')
+    img.src = 'C:\\Users\\A To Z\\Desktop\\image\\CV.jpg'
+    div.appendChild(img);
+}
